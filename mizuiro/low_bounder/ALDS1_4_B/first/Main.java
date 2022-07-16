498ad15ab008eac2bb99820caf4302a70a67f4da
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-07-16 18:52:42 +0900

    終了

--- mizuiro/low_bounder/ALDS1_4_B/first/Main.java
+++ mizuiro/low_bounder/ALDS1_4_B/first/Main.java
@@ -2,24 +2,6 @@ import java.util.*;
 // テストデータ取り出し→標準出力まで
 public class Main{
  
-    public static int listup(int[] S,int[] B){
-        int result=0;
-        int s_length = S.length;
-        int b_length=B.length;
-        Arrays.sort(S);
-        Arrays.sort(B);
-        System.out.print("S : ");
-        for(int j=0;j<s_length;j++){
-            System.out.print(S[j]+" ");
-        }
-        System.out.println("");
-        System.out.print("B :");
-        for(int j=0;j<b_length;j++){
-            System.out.print(B[j]+" ");
-        }
-        System.out.print("\n");
-        return result;
-    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // データ取得プロセス
@@ -37,21 +19,16 @@ public class Main{
         }
         // データ出力プロセス
         sc.close();
-        int k=listup(S,T);
-        System.out.println(k);
-        /**
-        System.out.print("S : ");
-        for(int j=0;j<n;j++){
-            System.out.print(S[j]+" ");
-        }
-        System.out.println("");
-        System.out.print("T : ");
+        Arrays.sort(S);
+        int result=0;
         for(int j=0;j<q;j++){
-            System.out.print(T[j]+" ");
+            Boolean checkresult;
+            checkresult=check(S,T[j]);
+            if(checkresult){
+                result = result + 1;
+            }
         }
-        System.out.print("\n");    
-        // System.out.println(line1);
-        **/
+        System.out.println(result);
     }
 
 }
