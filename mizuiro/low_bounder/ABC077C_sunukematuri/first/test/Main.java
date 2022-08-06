4bcbec341f70365c87e59633b78f46aea993ecfe
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-08-06 19:25:22 +0900

    フォルダの作成時従来「問題コード」だったが問題名も使うことにした

---
+++ mizuiro/low_bounder/ABC077C_sunukematuri/first/test/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        int testarray,testnumber;
+        Scanner sc = new Scanner(System.in);
+        testarray = sc.nextInt(); // 1行目 テストで扱う配列数
+        testnumber = sc.nextInt(); // 2行目 配列の内訳
+        for(int j=0;j<testarray;j++){
+            long num[] = new long[testnumber];
+            for(int k=0;k<testnumber;k=k+1){
+                num[k] = sc.nextLong();
+            }
+            Arrays.sort(num);
+            for(int k=0;k<testnumber;k=k+1){
+                System.out.print(num[testnumber]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
