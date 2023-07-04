b42b86348194ce29e82faeea72b8279e8b7f090b
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-07-04 17:53:17 +0900

    少しだけ

---
+++ mizuiro2_h20v/bitzenntansaku/ARC114A_noprime/prime1/Sample1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Sample1{
+    public static bool[] isPrime=new bool[51];
+    public static void main(String[] args){
+        System.out.printf("Hello World!!\n");
+// TODO: 一旦素数である->trueにセットする->後で篩い分け当てはまらない物をfalseとする
+// 2023-07-04 17:52:29
+        for(int j=0;j<51;j=j+1){
+            isPrime[j]=false;
+        }
+
+    }
+}
